924fd86e5beffb8f6c761e442c8abcd741da2633
gitdemonic <mydemonic@gmail.com>
2018-10-29 15:06:19 +0800

    FZF script: add optional feature     - No Preview:       $ ./fzf_Vim_Open.sh -np

--- script/fzf/fzf_Vim_Open.sh
+++ script/fzf/fzf_Vim_Open.sh
@@ -11,8 +11,11 @@ function ctrl_c() {
 
 # Search a file with fzf inside a Tmux pane and then open it in an editor
 fzf_then_open_in_editor() {
-    file=$(fzf-tmux -e --no-mouse --preview '[[ $(file --mime {}) =~ binary ]] && echo {} is a binary file || (highlight -O ansi -l {} || coderay {} || rougify {} || cat {}) 2> /dev/null')
-
+    if [ $1 == '-np' ]; then
+        file=$(fzf-tmux -e --no-mouse)
+    else
+        file=$(fzf-tmux -e --no-mouse --preview '[[ $(file --mime {}) =~ binary ]] && echo {} is a binary file || (highlight -O ansi -l {} || coderay {} || rougify {} || cat {}) 2> /dev/null')
+    fi
     # Open the file if it exists
 #: <<'END'
     while [ -n "$file" ]; do
@@ -32,4 +35,4 @@ fzf_preview() {
 
 #bind -x '"\C-t": fzf_then_open_in_editor'
 
-fzf_then_open_in_editor
+fzf_then_open_in_editor $1
